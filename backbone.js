bcb15b1fe22d3605f3fce3acbca13235b8be3b60
Graham Daws <graham.daws@gmail.com>
2012-02-01 00:52:09 +0000

    A model added to a collection inherits the collection's sync function

--- backbone.js
+++ backbone.js
@@ -649,10 +649,12 @@
       if (!(model instanceof Backbone.Model)) {
         var attrs = model;
         options.collection = this;
+        options.sync = this.sync;
         model = new this.model(attrs, options);
         if (!model._validate(model.attributes, options)) model = false;
       } else if (!model.collection) {
         model.collection = this;
+        model.sync = this.sync;
       }
       return model;
     },
